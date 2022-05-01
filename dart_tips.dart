ef24d15327c7251501a6e292d92c7c123dcf37c1
Régis Rommel Madeiro Sales <48102820+RegisSalesRA@users.noreply.github.com>
2022-05-01 18:56:41 -0300

    update tips

---
+++ dart_tips.dart
@@ -0,0 +1,13 @@
+## Fast review structures tips
+```dart
+variables always with camalcase
+functions always with snakecase
+files always with snakecase
+import names prefixes with snakecase
+class always with camelcase
+
+DON’T use prefix letters.
+```
+
+how you can know what type of from variable?
+EX: print("VariableName".runtimeType);
