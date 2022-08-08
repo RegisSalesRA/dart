3674dc4aa9fa47b87fa45691fe45d85b31d47d54
Régis Rommel Madeiro Sales <48102820+RegisSalesRA@users.noreply.github.com>
2022-08-08 12:37:04 -0300

    Update class.dart

--- object_oriented_programming/class.dart
+++ object_oriented_programming/class.dart
@@ -1,27 +1,44 @@
 class Food {
   String? nome;
   int? preco;
+  String? marca;
 
 // Metodo construtor completo
-/*
-  Food(String nome, int preco){
-    this.nome = nome;
-    this.preco = preco;
-  }
-*/
-
-// Método construtor simples com parametros posicionais
-Food(this.preco,this.nome) {}
-
-// Método construtor simples com parametros nomeados
-// Food({this.nome, this.preco});
-
-// Método com paramentros opcionais
-// Food([this.nome,this.preco]) {}
-
+Food(String nome, int preco, String marca){
+this.nome = nome;
+this.preco = preco;
+this.marca = marca;
 }
+var fruit = Food("Maçã",10,"brasil");
+print("Nome da fruta ${fruit.nome} e o preco ${fruit.preco} e a marca ${fruit.marca}");
+ 
+
+
+// Método construtor simples
+Food(this.nome,this.preco,this.marca); 
+var fruit = Food("Maçã",10,"brasil");
+print("Nome da fruta ${fruit.nome} e o preco ${fruit.preco} e a marca ${fruit.marca}");
+ 
+  
+// Método construtor simples com parametros nomeados
+ Food(this.nome, this.preco,{this.marca="Brasil"}); 
+var fruit = Food("Maçã",10 );
+print("Nome da fruta ${fruit.nome} e o preco ${fruit.preco} e a marca ${fruit.marca}");
+ 
+// Método construtor simples com parametros nomeados obrigatorios
+Food({required this.nome,required this.preco});
+var fruit = Food(nome:"Maçã",preco:10);
+print("Nome da fruta ${fruit.nome} e o preco ${fruit.preco} e a marca ${fruit.marca}");
+
+  
+// Método com paramentros posicionais
+Food(this.nome,this.preco,[this.marca = ""]) {}
+var fruit = Food( "Maçã", 10,"brasil");
+print("Nome da fruta ${fruit.nome} e o preco ${fruit.preco} e a marca ${fruit.marca}");
+}
+
 
 void main() {
-  var fruit = Food(10,"Acabaxi");
-  print("Nome da fruta ${fruit.nome} e o preco ${fruit.preco}");
-}
+  var fruit = Food( "Maçã", 10,"brasil");
+  print("Nome da fruta ${fruit.nome} e o preco ${fruit.preco} e a marca ${fruit.marca}");
+} 
