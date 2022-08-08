fe2e8240ddf6082d68546cdce5a1e4c0d47ab3bb
Régis Rommel Madeiro Sales <48102820+RegisSalesRA@users.noreply.github.com>
2022-08-08 10:56:09 -0300

    Update functions.dart

--- functions/functions.dart
+++ functions/functions.dart
@@ -5,9 +5,15 @@ void main() {
   int Function(int, int) soma = somaFn;
   print(soma(20, 313));
   print(somaFn(20, 313));
+  print(saudacoes());
 
 }
 
 int somaFn(int a, int b) {
   return a + b;
+}
+
+String saudacoes(){
+  DateTime agora = DateTime.now();
+  return agora.toString();
 }
