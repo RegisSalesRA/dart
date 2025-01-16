2a4e6b905f1a51d034d4bf9f597486a003ff4019
Régis Rommel <48102820+RegisSalesRA@users.noreply.github.com>
2025-01-16 14:55:52 -0300

    Create list.dart

---
+++ exercises/list/list.dart
@@ -0,0 +1,186 @@
+
+####Fácil
+  
+Filtrar números pares: Filtre os números pares da lista:
+
+dart
+Copiar
+Editar
+List<int> numeros = [1, 2, 3, 4, 5, 6];
+Somar elementos: Calcule a soma dos números na lista:
+
+dart
+Copiar
+Editar
+List<int> numeros = [10, 20, 30];
+Duplicar elementos: Crie uma nova lista duplicando cada número:
+
+dart
+Copiar
+Editar
+List<int> numeros = [1, 2, 3];
+Transformar em string: Converta todos os números da lista em strings:
+
+dart
+Copiar
+Editar
+List<int> numeros = [1, 2, 3];
+Ordenar a lista: Organize a lista em ordem crescente:
+
+dart
+Copiar
+Editar
+List<int> numeros = [4, 2, 5, 1];
+Remover números negativos: Filtre apenas os números positivos:
+
+dart
+Copiar
+Editar
+List<int> numeros = [-1, 2, -3, 4];
+Encontrar o maior número: Retorne o maior número da lista:
+
+dart
+Copiar
+Editar
+List<int> numeros = [5, 3, 9, 1];
+Verificar presença: Verifique se o número 7 está na lista:
+
+dart
+Copiar
+Editar
+List<int> numeros = [1, 2, 3, 7];
+Contar elementos: Conte o número de elementos da lista:
+
+dart
+Copiar
+Editar
+List<String> nomes = ['Ana', 'Pedro', 'João'];
+Criar uma lista de números: Crie uma lista com números de 1 a 10.
+
+#####  Médio
+
+Remover duplicados: Elimine os elementos repetidos da lista:
+
+dart
+Copiar
+Editar
+List<int> numeros = [1, 2, 2, 3, 3, 4];
+Filtrar palavras com mais de 4 letras:
+
+dart
+Copiar
+Editar
+List<String> palavras = ['dart', 'flutter', 'codigo'];
+Multiplicar todos os números por 3:
+
+dart
+Copiar
+Editar
+List<int> numeros = [2, 3, 4];
+Reverter a lista: Inverta a ordem dos elementos da lista:
+
+dart
+Copiar
+Editar
+List<int> numeros = [1, 2, 3];
+Separar pares e ímpares em duas listas:
+
+dart
+Copiar
+Editar
+List<int> numeros = [1, 2, 3, 4];
+Filtrar números divisíveis por 5:
+
+dart
+Copiar
+Editar
+List<int> numeros = [5, 10, 12, 15];
+Criar lista de strings formatadas: Adicione "Item " antes de cada número:
+
+dart
+Copiar
+Editar
+List<int> numeros = [1, 2, 3];
+Ordenar por comprimento da palavra:
+
+dart
+Copiar
+Editar
+List<String> palavras = ['casa', 'programar', 'sol'];
+Soma acumulativa: Retorne uma lista com a soma acumulativa dos números:
+
+dart
+Copiar
+Editar
+List<int> numeros = [1, 2, 3];
+Transformar lista de strings em lista de inteiros:
+
+dart
+Copiar
+Editar
+List<String> numeros = ['1', '2', '3'];
+
+#######Difícil
+  
+Remover números consecutivos repetidos:
+
+dart
+Copiar
+Editar
+List<int> numeros = [1, 2, 2, 3, 3, 4];
+Intercalar duas listas:
+
+dart
+Copiar
+Editar
+List<int> lista1 = [1, 2, 3];
+List<int> lista2 = [4, 5, 6];
+Encontrar o segundo maior número:
+
+dart
+Copiar
+Editar
+List<int> numeros = [10, 20, 30, 40];
+Agrupar números por paridade:
+
+dart
+Copiar
+Editar
+List<int> numeros = [1, 2, 3, 4];
+Remover palavras com vogais duplicadas:
+
+dart
+Copiar
+Editar
+List<String> palavras = ['casa', 'lua', 'banana'];
+Gerar todas as combinações possíveis:
+
+dart
+Copiar
+Editar
+List<int> numeros = [1, 2];
+Calcular diferenças consecutivas:
+
+dart
+Copiar
+Editar
+List<int> numeros = [10, 20, 30];
+Concatenar duas listas e ordenar:
+
+dart
+Copiar
+Editar
+List<int> lista1 = [3, 1];
+List<int> lista2 = [4, 2];
+Remover o menor e maior número:
+
+dart
+Copiar
+Editar
+List<int> numeros = [1, 2, 3, 4, 5];
+Criar sublistas com tamanho máximo de 2:
+
+dart
+Copiar
+Editar
+List<int> numeros = [1, 2, 3, 4];
